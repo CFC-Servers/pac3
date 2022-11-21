0aee8cd057216f2f693e3d4146cf4bcabdd12182
Brandon Sturgeon <brandon@brandonsturgeon.com>
2022-11-21 00:13:31 -0800

    [WIP] Add effects tests

---
+++ lua/tests/pac3/core/server/effects.lua
@@ -0,0 +1,59 @@
+return {
+    groupName = "Effects",
+    cases = {
+        {
+            name = "Creates all tables and receivers",
+            func = function()
+                expect( pac.EffectsBlackList ).to.exist()
+                expect( pac_loaded_particle_effects ).to.exist()
+
+                expect( net.Receivers["pac_request_precache"] ).to.exist()
+
+                expect( pac.PrecacheEffect ).to.exist()
+            end
+        },
+
+        -- pac.PrecacheEffect
+        {
+            name = "pac.PrecacheEffect precaches the given effect and broadcasts it to all clients",
+            func = function()
+                local precacheStub = stub( _G, "PrecacheParticleSystem" ).with( function( name )
+                    expect( name ).to.equal( "test_effect" )
+                end )
+
+                local startStub = stub( net, "Start" ).with( function( name )
+                    expect( name ).to.equal( "pac_effect_precached" )
+                end )
+
+                local writeStringStub = stub( net, "WriteString" ).with( function( name )
+                    expect( name ).to.equal( "test_effect" )
+                end )
+
+                local broadcastStub = stub( net, "Broadcast" )
+
+                pac.PrecacheEffect( "test_effect" )
+                expect( precacheStub ).to.haveBeenCalled()
+                expect( startStub ).to.haveBeenCalled()
+                expect( writeStringStub ).to.haveBeenCalled()
+                expect( broadcastStub ).to.haveBeenCalled()
+            end
+        },
+
+        -- pac_request_precache receiver
+        {
+            name = "pac_request_precache receiver queues and calls pac.PrecacheEffect on the given effect for the given player",
+            func = function()
+            end
+        },
+        {
+            name = "pac_request_precache receiver does not accept more than 50 queued effects for a single player",
+            func = function()
+            end
+        },
+        {
+            name = "pac_request_precache receiver does not accept blacklisted effects",
+            func = function()
+            end
+        },
+    }
+}
