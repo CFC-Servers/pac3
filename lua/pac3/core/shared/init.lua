94a6443f3a4529dccd03fc331983362f40626f0c
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2026-08-28 09:16:16 -0500

    Move init spawn check to server

--- lua/pac3/core/shared/init.lua
+++ lua/pac3/core/shared/init.lua
@@ -61,16 +61,4 @@ do
 	for i, val in ipairs(tohash) do
 		pac.BlacklistedParticleSystems[val] = true
 	end
-
-	hook.Add("PlayerInitialSpawn", "pac_initial_spawn", function(ply)
-		local id = "pac_initial_spawn" .. ply:UniqueID()
-		hook.Add( "SetupMove", id, function(movingPly, _, cmd)
-			if not ply:IsValid() then
-				hook.Remove("SetupMove", id)
-			elseif movingPly == ply and not cmd:IsForced() then
-				hook.Run("pac_initial_spawn", ply)
-				hook.Remove("SetupMove", id)
-			end
-		end)
-	end)
 end
